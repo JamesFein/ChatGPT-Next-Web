7267b75fae53ff21ed0d9e40fe2b23d56445b294
JamesFein <137151155+JamesFein@users.noreply.github.com>
2024-07-31 11:43:36 +0800

    Update constant.ts
    Added support for claude

--- app/constant.ts
+++ app/constant.ts
@@ -12,7 +12,7 @@ export const STABILITY_BASE_URL = "https://api.stability.ai";
 
 export const DEFAULT_API_HOST = "https://api.nextchat.dev";
 export const OPENAI_BASE_URL = "https://api.deepbricks.ai";
-export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
+export const ANTHROPIC_BASE_URL = "https://api.deepbricks.ai";;
 
 export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
 
