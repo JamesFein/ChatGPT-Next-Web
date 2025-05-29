64c14f947a1248a3ef22e3985aa48941a015a847
JamesFein <nlpdaren@gmail.com>
2025-05-29 12:51:36 +0800

    把所有模型都设置成了视觉模型，让传图按钮永存

--- app/utils.ts
+++ app/utils.ts
@@ -9,8 +9,6 @@ import {
 } from "./constant";
 // import { fetch as tauriFetch, ResponseType } from "@tauri-apps/api/http";
 import { fetch as tauriStreamFetch } from "./utils/stream";
-import { VISION_MODEL_REGEXES, EXCLUDE_VISION_MODEL_REGEXES } from "./constant";
-import { useAccessStore } from "./store";
 import { ModelSize } from "./typing";
 
 export function trimTopic(topic: string) {
@@ -281,15 +279,8 @@ export function getMessageImages(message: RequestMessage): string[] {
 }
 
 export function isVisionModel(model: string) {
-  const visionModels = useAccessStore.getState().visionModels;
-  const envVisionModels = visionModels?.split(",").map((m) => m.trim());
-  if (envVisionModels?.includes(model)) {
-    return true;
-  }
-  return (
-    !EXCLUDE_VISION_MODEL_REGEXES.some((regex) => regex.test(model)) &&
-    VISION_MODEL_REGEXES.some((regex) => regex.test(model))
-  );
+  // 全部模型都视为视觉模型，始终显示图片上传按钮
+  return true;
 }
 
 export function isDalle3(model: string) {
